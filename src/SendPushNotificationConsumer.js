bad9800c9597abd5cdeb2e18bc5f428708ceca09
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2020-01-04 17:17:25 -0300

    feat: learning kafka

---
+++ src/SendPushNotificationConsumer.js
@@ -0,0 +1,26 @@
+const uuid = require('uuid');
+const kafka = require('kafka-node');
+const ConsumerGroup = kafka.ConsumerGroup;
+const OPTIONS = {
+  kafkaHost: '127.0.0.1:9092',
+  groupId: `SEND-PUSHNOTIFICATION-CONSUMER`,
+  protocol: ['roundrobin'],
+};
+
+const TOPICS = ['LOJA_NOVO_PEDIDO']
+
+class SendPushNotificationConsumer {
+	constructor() {
+    this.consumer = new ConsumerGroup(OPTIONS, TOPICS)
+    this.onMessage();
+  }
+
+  onMessage() {
+    this.consumer.on('message', (message) => {
+      console.log(`[${this.consumer.options.groupId} : ${JSON.stringify(message)}]`)
+    })
+  }
+}
+
+
+new SendPushNotificationConsumer()
